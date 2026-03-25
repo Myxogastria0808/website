d16c08b225f88cb92c66247c4f5abdcc5c6064e2
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-26 00:46:48 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Myxogastria0808's website";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # JavaScript / TypeScript
+            nodejs
+            corepack
+            # Elm
+            elmPackages.elm
+          ];
+        };
+      }
+    );
+}
