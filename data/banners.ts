e02544fb214205e978984f83fe53266928b4004b
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-06-11 01:51:53 +0900

    chore: convert banners from png to webp

--- data/banners.ts
+++ data/banners.ts
@@ -7,27 +7,27 @@ export type Banner = {
 export const BANNERS: Banner[] = [
   {
     href: "https://mizuame.works",
-    src: "/banners/mizuame_banner.png",
+    src: "/banners/mizuame_banner.webp",
     alt: "Portfolio of mizuame",
   },
   {
     href: "https://www.ak47.work/",
-    src: "/banners/ak47_banner.png",
+    src: "/banners/ak47_banner.webp",
     alt: "Website of ak47",
   },
   {
     href: "https://blog.whatacotton.com/",
-    src: "banners/whatacotton_banner.png",
+    src: "banners/whatacotton_banner.webp",
     alt: "Blog of whatacotton",
   },
   {
     href: "https://www.powerfulfamily.net/",
-    src: "banners/powerfulfamily_banner.png",
+    src: "banners/powerfulfamily_banner.webp",
     alt: "Website of masebb",
   },
   {
     href: "https://sites.google.com/view/happy-busy/",
-    src: "banners/happy-busy.png",
+    src: "banners/happy-busy.webp",
     alt: "Website of happy-busy",
   },
 ];
